1ee9e86fede24386ddd00c8054dc7a24c3da7c42
Luiz Fernando <nandomagalhaes074@gmail.com>
2025-04-17 15:22:57 -0300

    Create Task Model

--- src/main/java/com/luizfnando/todosimple/models/User.java
+++ src/main/java/com/luizfnando/todosimple/models/User.java
@@ -1,12 +1,7 @@
 package com.luizfnando.todosimple.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -16,7 +11,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "User.TABLE_NAME")
+@Table(name = User.TABLE_NAME)
 public class User {
 
     public interface CreateUser{}
@@ -42,7 +37,9 @@ public class User {
     @Size(groups = {CreateUser.class, UpdateUser.class},min = 6, max = 60)
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>();
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
 
     public User() {
@@ -76,6 +73,14 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 
     @Override
